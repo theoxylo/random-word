99e86a6cc7f6ee4f0bb9e6e8cb0689d76fe6d181
Theoxylo <theoxylo@yahoo.com>
2020-07-13 15:14:38 -0700

    Add files via upload

---
+++ src/main/java/com/theoxylo/wordserver/Word.java
@@ -0,0 +1,14 @@
+package com.theoxylo.wordserver;
+
+public class Word {
+    
+    String word;
+
+    public Word(String s) {
+        word = s;
+    }
+    
+    public String getWord() {
+        return word;
+    }
+}
---
+++ src/main/java/com/theoxylo/wordserver/WordController.java
@@ -0,0 +1,29 @@
+package com.theoxylo.wordserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping(value="api")
+public class WordController {
+
+	@Autowired 
+	private WordService wordService;
+
+	@GetMapping(value="/word")
+	public ResponseEntity<Word> getRandomWord() {
+		Word word = wordService.getRandomWord();
+		return ResponseEntity.ok(word);
+	}
+
+}
