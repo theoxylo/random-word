c486d2f7d28efdc2e25a89d7c592eb35e78539c9
toneill <toneill@localhost>
2021-01-07 09:23:15 -0800

    added file loading example, no cache

--- src/main/java/com/theoxylo/wordserver/WordService.java
+++ src/main/java/com/theoxylo/wordserver/WordService.java
@@ -2,6 +2,9 @@ package com.theoxylo.wordserver;
 
 import java.util.List;
 import java.util.Collection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
@@ -11,6 +14,8 @@ import com.theoxylo.wordserver.KafkaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 @Service
 public class WordService {
@@ -48,6 +53,26 @@ public class WordService {
 		}		
 		log.info("all words: " + words);
 		return list;
+	}
+	
+	public Word getRandomWordFromFile() {
+        List<String> _words = new ArrayList<String>();
+		try {
+            String _wordsFile = "words_alpha.txt";
+			Resource resource = new ClassPathResource(_wordsFile);
+			BufferedReader in = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+			while (in.ready()) {
+				String word = in.readLine();
+				_words.add(word);
+				//kafkaService.send(new Word(word)); // to much console logging, 370k words
+			}
+			System.out.println("word count: " + _words.size());
+		}
+		catch (IOException e) {
+			System.out.println(e);
+		}
+		int randomIndex = new Random().nextInt(_words.size());
+		return new Word(_words.get(randomIndex));	
 	}
 
 }
