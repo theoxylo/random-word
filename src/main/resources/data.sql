49934498a7a0439a03aca0845abfaf5bd429cc5e
toneill <toneill@localhost>
2021-01-09 14:40:35 -0800

    minimal sample data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,2 @@
-drop table if exists word;
-
-create table word (
-	id int auto_increment primary key,
-	word VARCHAR(128) not null,
-	definition VARCHAR(1024) default null
-);
-
-insert into word (word) values ('test'), ('orange');
-insert into word (word) values ('pineapple'), ('apple');
-insert into word (word) values ('banana'), ('grape');
+insert into word (text) values ('banana'), ('orange');
 
